de55512e765662914245de8147537459e0160c6f
Michael D. Bright <michaeldbright@gmail.com>
2016-10-15 22:25:00 -0700

    Tis is the passing commit foc
    g

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,18 @@
 #write your code here
+def ftoc(temp)
+  (temp-32) * (5.0/9)
+end
+
+def ctof(temp)
+  (temp* (9.0/5)) +32
+end
+
+ftoc(32)
+ftoc(212)
+ftoc(98.6)
+ftoc(68)
+
+ctof(0)
+ctof(100)
+ctof(20)
+ctof(37)
